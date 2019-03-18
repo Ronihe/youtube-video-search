6d2860ac02554831ab575b9c545e941b4446c4c1
Ronihe <herongrong2011@gmail.com>
2019-03-17 23:29:50 -0700

    removed the react logo

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-
-class Video extends Component {
+import './VideoCard.css';
+import { Link } from 'react-router-dom';
+class VideoCard extends Component {
   render() {
     console.log(this.props);
-    return <div className="Video" />;
+    return (
+      <div className="VideoCard">
+        <Link to={'/' + this.props.videoId}>
+          <img src={this.props.src} alt={this.props.title} />
+          <div>
+            <h4> {this.props.title}</h4>
+            <p>
+              <small>{this.props.publishedAt}</small>
+            </p>
+            <p>
+              {' '}
+              <small>{this.props.description}</small>
+            </p>
+          </div>
+        </Link>
+      </div>
+    );
   }
 }
 
-Video.defaultProps = {};
+VideoCard.defaultProps = {};
+VideoCard.propTypes = {};
 
-Video.propTypes = {};
-
-export default Video;
+export default VideoCard;
