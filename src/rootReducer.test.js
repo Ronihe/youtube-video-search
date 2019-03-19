b91fb98c90886a57c6ba8440a7bfa56c494b0e51
Ronihe <herongrong2011@gmail.com>
2019-03-19 15:35:34 -0700

    rootReducer testing is done

--- src/rootReducer.test.js
+++ src/rootReducer.test.js
@@ -2,11 +2,98 @@ import reducer from './rootReducer';
 import * as types from './actions/types';
 
 describe('videos reducers', () => {
+  const q = 'cat';
+  let nextPageToken = 'testingToken';
+  const videos = [{ test1: 'test1' }, { test2: 'test2' }];
+
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual([
-      { q: '', nextPageToken: '', page: 1, videos: [] }
-    ]);
+    expect(reducer(undefined, {})).toEqual({
+      q: '',
+      nextPageToken: '',
+      page: 1,
+      videos: []
+    });
   });
 
-  // it('should return initial videos from the youtube API', () => {});
+  it('should handle GET_Q', () => {
+    expect(
+      reducer(
+        {
+          q: '',
+          nextPageToken: '',
+          page: 1,
+          videos: []
+        },
+        {
+          type: types.GET_Q,
+          q,
+          nextPageToken,
+          videos
+        }
+      )
+    ).toEqual({
+      q,
+      page: 1,
+      nextPageToken,
+      videos
+    });
+  });
+
+  it('should handle GET_NEW_VIDEOS', () => {
+    nextPageToken = 'newTestingToken';
+    expect(
+      reducer(
+        {
+          q,
+          page: 1,
+          nextPageToken,
+          videos
+        },
+        { type: types.GET_NEW_VIDEOS, nextPageToken, videos }
+      )
+    ).toEqual({
+      q,
+      page: 2,
+      nextPageToken,
+      videos: [...videos, ...videos]
+    });
+  });
+
+  it('should handle PREVIOUS_PAGE', () => {
+    expect(
+      reducer(
+        {
+          q,
+          page: 2,
+          nextPageToken,
+          videos: [...videos, ...videos]
+        },
+        { type: types.PREVIOUS_PAGE, nextPageToken }
+      )
+    ).toEqual({
+      q,
+      page: 1,
+      nextPageToken,
+      videos: [...videos, ...videos]
+    });
+  });
+
+  it('should handle NEXT_PAGE', () => {
+    expect(
+      reducer(
+        {
+          q,
+          page: 1,
+          nextPageToken,
+          videos: [...videos, ...videos]
+        },
+        { type: types.NEXT_PAGE, nextPageToken }
+      )
+    ).toEqual({
+      q,
+      page: 2,
+      nextPageToken,
+      videos: [...videos, ...videos]
+    });
+  });
 });
