8650650e5eda3a80468892df63d87191634ecfff
Ronihe <herongrong2011@gmail.com>
2019-03-19 11:26:42 -0700

    creation test is done

--- src/actions/videos.test.js
+++ src/actions/videos.test.js
@@ -1,1 +1,43 @@
-import { getNextVideos, getVideos, getPreviousVideo } from './videos';
+import * as actions from './videos';
+import * as types from './types';
+
+//Testing actions
+
+describe('actions', () => {
+  const q = 'cat';
+  const nextPageToken = 'testToken';
+  const videos = [{ test1: 'test1' }, { test2: 'test2' }];
+
+  it('should create an action to get initial videos from youtube API', () => {
+    const expectedAction = {
+      type: types.GET_Q,
+      q,
+      nextPageToken,
+      videos
+    };
+    expect(actions.getInitialVideos(q, nextPageToken, videos)).toEqual(
+      expectedAction
+    );
+  });
+
+  it('should create an action to get new videos from youtube API with nextPageToken', () => {
+    const expectedAction = {
+      type: types.GET_NEW_VIDEOS,
+      nextPageToken,
+      videos
+    };
+    expect(actions.getNewVideos(nextPageToken, videos)).toEqual(expectedAction);
+  });
+  it('should create an action to get previous videos from state', () => {
+    const expectedAction = {
+      type: types.PREVIOUS_PAGE
+    };
+    expect(actions.previousVideos()).toEqual(expectedAction);
+  });
+  it('should create an action to get next videos from state', () => {
+    const expectedAction = {
+      type: types.NEXT_PAGE
+    };
+    expect(actions.getNextVideos()).toEqual(expectedAction);
+  });
+});
