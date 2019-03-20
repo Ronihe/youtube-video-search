570f9d823d01f6335873e24ec4acccec3020a3b2
Ronihe <herongrong2011@gmail.com>
2019-03-19 17:18:02 -0700

    Updates

---
+++ precache-manifest.f5389ccb9c10da66693b929a6933d5ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4927bc3ee3011db58e4",
+    "url": "/youtube-video-search2/static/js/runtime~main.9272085e.js"
+  },
+  {
+    "revision": "75aee50df6141f628cd5",
+    "url": "/youtube-video-search2/static/js/main.1dea07d3.chunk.js"
+  },
+  {
+    "revision": "0a7d79e6cc5e186e61f6",
+    "url": "/youtube-video-search2/static/js/2.c16fed35.chunk.js"
+  },
+  {
+    "revision": "75aee50df6141f628cd5",
+    "url": "/youtube-video-search2/static/css/main.971411fc.chunk.css"
+  },
+  {
+    "revision": "6709e769eb2c5be449d1655f7f6a4b47",
+    "url": "/youtube-video-search2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Ronihe/youtube-video-search/precache-manifest.2aa459d5b89d138077ad2e7aef103f7a.js"
+  "/youtube-video-search2/precache-manifest.f5389ccb9c10da66693b929a6933d5ad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Ronihe/youtube-video-search/index.html", {
+workbox.routing.registerNavigationRoute("/youtube-video-search2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
