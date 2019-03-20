77b001d5ef221f4b458e562015589a70c2c80989
Ronihe <herongrong2011@gmail.com>
2019-03-19 17:01:53 -0700

    Updates

---
+++ precache-manifest.b523e5e83583ed29d93a55d4943cc1e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fdd8a19786cff1c478d",
+    "url": "/youtube-video-search/static/js/runtime~main.0f4e6c20.js"
+  },
+  {
+    "revision": "549107aa3081024e1c86",
+    "url": "/youtube-video-search/static/js/main.602e7d72.chunk.js"
+  },
+  {
+    "revision": "0a7d79e6cc5e186e61f6",
+    "url": "/youtube-video-search/static/js/2.c16fed35.chunk.js"
+  },
+  {
+    "revision": "549107aa3081024e1c86",
+    "url": "/youtube-video-search/static/css/main.971411fc.chunk.css"
+  },
+  {
+    "revision": "c3987b3cc547640ae3a7de8cb56d28b1",
+    "url": "/youtube-video-search/index.html"
+  }
+];
